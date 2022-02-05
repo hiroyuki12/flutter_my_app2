bfd53f8a8f939e5c8040791ece9e736cb3a58ed7
hiroyuki12 <hiroyuki12@gmail.com>
2022-02-05 22:00:12 +0900

    Update bundle identifier.

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             CupertinoButton(
-              child: Text("Push Qiita"),
+              child: Text("Push Qiita(API) Flutter"),
               onPressed: () => 
                 Navigator.push(
                   context, MaterialPageRoute(builder: (context) => Qiita())
