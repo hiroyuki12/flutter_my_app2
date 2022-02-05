9c99307d6d21f65dc29625e0fc17e67253b694ef
hiroyuki12 <hiroyuki12@gmail.com>
2022-02-05 18:49:29 +0900

    Add QiitaScreen, RaiseButton Navigator Push Qiita.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'pages/QiitaScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,14 +8,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      //theme: ThemeData(
-      //  primarySwatch: Colors.blue,
-      //),
       theme: ThemeData.dark(),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -25,79 +20,13 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class Item {
-  Item({
-    this.title,
-    this.profileImageUrl,
-  });
-
-  final String? title;
-  final String? profileImageUrl;
-}
-
-class Issue {
-  Issue({
-    this.title,
-    this.avatarUrl,
-  });
-
-  final String? title;
-  final String? avatarUrl;
-}
-
 class _MyHomePageState extends State<MyHomePage> {
-  String _data = '';
-  List<String> _titles = <String>[];
-  List<Issue> _issues = <Issue>[];
-  List<Item> _qiitaItems= <Item>[];
-
-  @override
-  void initState() {
-    super.initState();
-    _load();
-  }
-
-  Future<void> _load() async {
-    //String url = 'https://api.github.com/repositories/31792824/issues';
-    //final res = await http.get(Uri.parse(url));
-    //final data = json.decode(res.body);
-    String url = 'http://qiita.com/api/v2/items';
-    final res = await http.get(Uri.parse(url));
-    final data = json.decode(res.body);
-
-    setState(() {
-      //_data = res.body;
-
-      final qiitaItems = data as List;
-      qiitaItems.forEach((dynamic element) {
-        final qiitaItem = element as Map;
-        _qiitaItems.add(Item(
-          title: qiitaItem['title'] as String,
-          profileImageUrl: qiitaItem['user']['profile_image_url'] as String,
-        ));
-      });
-
-      /*
-      final issues = data as List;
-      issues.forEach((dynamic element) {
-        final issue = element as Map;
-        _titles.add(issue['title'] as String);
-        _issues.add(Issue(
-          title: issue['title'] as String,
-          avatarUrl: issue['user']['avatar_url'] as String,
-        ));
-      });
-      */
-
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -105,33 +34,22 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      
-      body: ListView.builder(
-        itemBuilder: (BuildContext context, int index) {
-          //if (index >= _qiitaItems.length) {
-          //  return null;
-          //}
-
-          final qiitaItem = _qiitaItems[index];
-          return ListTile(
-            leading: ClipOval(
-              child: Image.network(qiitaItem.profileImageUrl!),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text('Qiita'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Qiita()),
+                );
+              },
             ),
-            title: Text(qiitaItem.title!),
-          );
-
-          /*
-          final issue = _issues[index];
-          return ListTile(
-            leading: ClipOval(
-              child: Image.network(issue.avatarUrl!),
-            ),
-            title: Text(issue.title!),
-          );
-          */
-        },
+          ],
+        ),
       ),
-      
     );
   }
 }
---
+++ lib/pages/QiitaScreen.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Qiita extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _State();
+  }
+}
+
+class Item {
+   Item({
+     this.title,
+     this.profileImageUrl,
+   });
+
+   final String? title;
+   final String? profileImageUrl;
+ }
+
+class _State extends State<Qiita> {
+  List<Item> _items = <Item>[];
+  @override
+  void initState() {
+    super.initState();
+    _load();
+  }
+  
+  // This widget is the root of your application.
+  Future<void> _load() async {
+    String url = 'http://qiita.com/api/v2/items';
+    final res = await http.get(Uri.parse(url));
+    final data = json.decode(res.body);
+    setState(() {
+      final issues = data as List;
+      issues.forEach((dynamic element) {
+        final issue = element as Map;
+        _items.add(Item(
+          title: issue['title'] as String,
+          profileImageUrl: issue['user']['profile_image_url'] as String,
+        ));
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Qiita"),
+      ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          if (index >= _items.length) {
+            //return null;
+            return ListTile(
+              title: Text(""),
+            );
+          }
+
+          final issue = _items[index];
+          return ListTile(
+            leading: ClipOval(
+              child: Image.network(issue.profileImageUrl!),
+            ),
+            title: Text(issue.title!),
+          );
+        },
+      ),
+    );
+  }
+}
