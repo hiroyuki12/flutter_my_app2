8eb43c86eaffed0c72db74b2ef552f1d19de5e9a
hiroyuki12 <hiroyuki12@gmail.com>
2022-02-06 22:21:10 +0900

    Add CupertinoButton With color, Disabled.

--- lib/pages/CupertinoButton.dart
+++ lib/pages/CupertinoButton.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
+import 'DarkModeColor.dart';
+
 class MyCupertinoButton extends StatefulWidget {
   @override
     State<StatefulWidget> createState() {
@@ -10,18 +12,69 @@ class MyCupertinoButton extends StatefulWidget {
 class _State extends State<MyCupertinoButton> {
   @override
   Widget build(BuildContext context) {
+    isDarkMode = true;  // switch darkMode
     return CupertinoPageScaffold(
+      backgroundColor: isDarkMode ? darkModeBackColor : backColor,  //white , darkMode=black
       navigationBar: CupertinoNavigationBar(
-        middle: Text("CupertinoButton", style: new TextStyle(color: CupertinoColors.white)),
-        backgroundColor: const Color(0xff333333),
+        middle: Text("CupertinoButton", style: _buildTextStyle()),
+        backgroundColor: isDarkMode ? darkModeBackColor : backColor,  //white , darkMode=black
       ),
       child: Center(
-        child: CupertinoButton(
-          onPressed: () {
-          },
-          child: Text("Tap me"),
+        child: Column(
+          children: <Widget>[
+            CupertinoButton(
+              //color: CupertinoColors.activeBlue,
+              //borderRadius: new BorderRadius.circular(30.0),
+              onPressed: () {
+              },
+              child: Text('No color', style: _buttonTextStyleNoBackground),
+            ),
+            SizedBox(height: 32,),
+            CupertinoButton(
+              color: CupertinoColors.activeBlue,  //backgroundColor
+              borderRadius: new BorderRadius.circular(30.0),
+              onPressed: () {
+              },
+              child: Text('With color', style: _buttonTextStyleWithBagckground),
+            ),
+            SizedBox(height: 32,),
+            CupertinoButton(
+              color: CupertinoColors.activeBlue,  //backgroundColor
+              borderRadius: new BorderRadius.circular(30.0),
+              disabledColor: CupertinoColors.systemGrey,
+              onPressed: null,
+              child: Text('Disabled', style: _buttonTextStyleWithBagckground),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+
+var myTextStyle = new TextStyle();
+TextStyle _buildTextStyle() {
+  return myTextStyle = new TextStyle(
+  fontWeight: FontWeight.w100,
+  decoration: TextDecoration.none,
+  fontSize: 16,
+  // color: CupertinoColors.white
+  color: isDarkMode ? darkModeForeColor : foreColor,  //black , darkMode=white
+  );
+}
+
+var _buttonTextStyleWithBagckground = new TextStyle(
+  //fontWeight: FontWeight.w100,
+  decoration: TextDecoration.none,
+  fontSize: 16,
+  color: CupertinoColors.white
+);
+
+TextStyle  _buttonTextStyleNoBackground = new TextStyle(
+  fontWeight: FontWeight.w300,
+  decoration: TextDecoration.none,
+  fontSize: 16,
+  // color: CupertinoColors.white
+  color: CupertinoColors.activeBlue,  //black , darkMode=white
+);
