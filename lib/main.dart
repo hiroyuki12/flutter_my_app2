52a4a66c5da1afa1f77344ef30ba78122a9314fa
hiroyuki12 <hiroyuki12@gmail.com>
2022-02-10 21:21:13 +0900

    Add link main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -44,14 +44,50 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             CupertinoButton(
-              child: Text("Push Qiita(API)"),
+              child: Text("flutter.dev(外部サイト)"),
+              onPressed: () =>
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                      MyCupertinoWebView(
+                        url: "https://flutter.dev",
+                        title: "flutter.dev"),
+                  ),
+                ),
+            ),
+            CupertinoButton(
+              child: Text("環境構築(外部サイト)"),
+              onPressed: () =>
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                      MyCupertinoWebView(
+                        url: "https://mbp.hatenablog.com/entry/2019/12/29/000000?_ga=2.97341151.1500483094.1644030611-1356087000.1644030611",
+                        title: "Flutter 入門"),
+                  ),
+                ),
+            ),
+            CupertinoButton(
+              child: Text("flutter_my_app2(外部サイト)"),
+              onPressed: () =>
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                      MyCupertinoWebView(
+                        url: "https://mbp.hatenablog.com/entry/2022/02/05/143159",
+                        title: "flutter_my_app2"),
+                  ),
+                ),
+            ),
+            CupertinoButton(
+              child: Text("Qiita(API)"),
               onPressed: () =>
                 Navigator.push(
                   context, MaterialPageRoute(builder: (context) => Qiita())
                 ),
             ),
             CupertinoButton(
-              child: Text("Push Zenn(外部サイト)"),
+              child: Text("Zenn(Flutter) 外部サイト"),
               onPressed: () =>
                 Navigator.of(context).push(
                   MaterialPageRoute(
@@ -63,19 +99,19 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
             ),
             CupertinoButton(
-              child: Text("Push Qiita(外部サイト)"),
+              child: Text("Qiita(Flutter) 外部サイト"),
               onPressed: () =>
                 Navigator.of(context).push(
                   MaterialPageRoute(
                     builder: (BuildContext context) =>
                       MyCupertinoWebView(
-                        url: "https://qiita.com/tags/react",
+                        url: "https://qiita.com/tags/flutter",
                         title: "Qiita Flutterの記事一覧"),
                   ),
                 ),
             ),
             CupertinoButton(
-              child: Text("Push はてなブログタグ(外部サイト)"),
+              child: Text("はてなブログタグ(外部サイト)"),
               onPressed: () =>
                 Navigator.of(context).push(
                   MaterialPageRoute(
@@ -87,7 +123,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
             ),
             CupertinoButton(
-              child: Text("Push Cupertino Menu ->"),
+              child: Text("Cupertino Menu ->"),
               onPressed: () => 
                 Navigator.push(
                   context, MaterialPageRoute(builder: (context) => CupertinoMenu())
