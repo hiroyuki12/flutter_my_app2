fd6d1bb9454b0df82ad013c959830aff03e216bb
hiroyuki12 <hiroyuki12@gmail.com>
2022-02-13 21:50:03 +0900

    Update CupertinoFirebase Add schools Firestore.

--- lib/pages/CupertinoFirebase.dart
+++ lib/pages/CupertinoFirebase.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'DarkModeColor.dart';
+import 'package:flutter/foundation.dart';
 
 class CupertinoFirebase extends StatefulWidget {
   @override
@@ -13,6 +14,7 @@ class CupertinoFirebase extends StatefulWidget {
 }
 
 class _State extends State<CupertinoFirebase> {
+
   final FirebaseAuth auth = FirebaseAuth.instance;
 
   //DatabaseReference ref = FirebaseDatabase.instance.reference();
@@ -38,11 +40,19 @@ class _State extends State<CupertinoFirebase> {
               //color: CupertinoColors.activeBlue,
               //borderRadius: new BorderRadius.circular(30.0),
               onPressed: () async {
-                    final UserCredential result =
+                    /*final UserCredential result =
                         await auth.createUserWithEmailAndPassword(     
                       email: "a@a.co.jp",
                       password: "bbbbbb",
+                    );*/
+
+                    final schoolRepository = SchoolRepository();
+                    final insertSchool = new School(
+                      name:"サンプル学校",
+                      updatedAt: DateTime.now(),
+                      createdAt: DateTime.now()
                     );
+                    final documentId = await schoolRepository.insert(insertSchool);
               },
               child: Text('Cupertino Firebase', style: _buildTextStyle()),
             ),
@@ -59,4 +69,43 @@ TextStyle _buildTextStyle() {
   fontSize: 16,
   color: isDarkMode ? darkModeForeColor : foreColor,  //black , darkMode=white
   );
+}
+
+/// 学校を扱うリポジトリ
+class SchoolRepository {
+  final schoolsManager = FirebaseFirestore.instance.collection('schools');
+  /// 学校情報を保存する
+  Future<String> insert(School school) async {
+    final data = await schoolsManager.add(school.toJson());
+    return data.id;
+  }
+}
+
+@immutable
+class School {
+  final String name;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+  final DateTime? deletedAt;
+
+  School({
+    required this.name,
+    required this.createdAt,
+    required this.updatedAt,
+    this.deletedAt,
+  });
+
+  //DartのオブジェクトからFirebaseへ渡す際の変換処理
+  Map<String, Object?> toJson() {
+    Timestamp? deletedTimestamp;
+    if (deletedAt != null) {
+      deletedTimestamp = Timestamp.fromDate(deletedAt!);
+    }
+    return {
+      'name': name,
+      'createdAt': Timestamp.fromDate(createdAt), //DartのDateTimeからFirebaseのTimestampへ変換
+      'updatedAt': Timestamp.fromDate(updatedAt), //DartのDateTimeからFirebaseのTimestampへ変換
+      'deletedAt': deletedTimestamp
+    };
+  }
 }
