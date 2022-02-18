e105c2ed3052d1bacadea44d948b99fff1cddd55
hiroyuki12 <hiroyuki12@gmail.com>
2022-02-18 23:08:46 +0900

    Update Twitter get user_timeline show text.

--- lib/pages/TwitterScreen.dart
+++ lib/pages/TwitterScreen.dart
@@ -36,7 +36,6 @@ class MyWidget extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyWidget> {
-  final controller = TextEditingController();
   final platform = oauth1.Platform(
     'https://api.twitter.com/oauth/request_token',
     'https://api.twitter.com/oauth/authorize',
@@ -47,19 +46,10 @@ class _MyWidgetState extends State<MyWidget> {
     'Twitter API Key をここに設定',
     'Twitter API Key Secret をここに設定',
   );
-  late final auth = oauth1.Authorization(clientCredentials, platform);
-  oauth1.Credentials? tokenCredentials;
 
   @override
   void initState() {
     super.initState();
-
-    // CallbackURLを"oob"とすることでPINでの認証とできる
-    auth.requestTemporaryCredentials('oob').then((res) {
-      tokenCredentials = res.credentials;
-      // launch() で ログイン用URLを開く
-      launch(auth.getResourceOwnerAuthorizationURI(tokenCredentials!.token));
-    });
   }
 
   @override
@@ -69,31 +59,22 @@ class _MyWidgetState extends State<MyWidget> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // ログイン後に表示されたPINを入力
-            TextFormField(
-              controller: controller,
-            ),
             ElevatedButton(
               onPressed: () async {
-                // 入力されたPINを元に Access Token を取得
-                final pin = controller.text;
-                final verifier = pin;
-                final res = await auth.requestTokenCredentials(
-                  tokenCredentials!,
-                  verifier,
-                );
-                print('Access Token: ${res.credentials.token}');
-                print('Access Token Secret: ${res.credentials.tokenSecret}');
-
-                // 取得した Access Token を使ってAPIにリクエストできる
+                // Access Token を使ってAPIにリクエストできる
                 final client = oauth1.Client(
                   platform.signatureMethod,
                   clientCredentials,
-                  res.credentials,
+                  oauth1.Credentials(
+                    'Twitter Access Token をここに設定',
+                    'Twitter Access Token Secret をここに設定',
+                  ),
                 );
                 final apiResponse = await client.get(
                   Uri.parse(
-                      'https://api.twitter.com/1.1/statuses/home_timeline.json?count=1'),
+                      //'https://api.twitter.com/1.1/statuses/home_timeline.json?count=1'),
+                      //'https://api.twitter.com/1.1/search/tweets.json?q=flutter&lang=ja&count=1'),
+                      'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterjp&count=2'),
                 );
 
                 final data = json.decode(apiResponse.body);
@@ -103,12 +84,13 @@ class _MyWidgetState extends State<MyWidget> {
                   final issue = element as Map;
                   _items.add(Item(
                     id_str: issue['id_str'] as String,
-                    text: issue['text'] as String,
+                    text:   issue['text'] as String,
                   ));
                 });
 
                 //print(apiResponse.body);
                 print(_items[0].text);
+                print(_items[1].text);
               },
               child: Text('OK'),
             ),
