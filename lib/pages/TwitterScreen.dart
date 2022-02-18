cb49e72ff39dc5ec934e664b1f77bb3258133744
hiroyuki12 <hiroyuki12@gmail.com>
2022-02-19 00:22:27 +0900

    Update Twitter add setState show text page.

--- lib/pages/TwitterScreen.dart
+++ lib/pages/TwitterScreen.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:oauth1/oauth1.dart' as oauth1;
 import 'package:url_launcher/url_launcher.dart';
@@ -36,6 +37,8 @@ class MyWidget extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyWidget> {
+  List<Item> _items = <Item>[];
+
   final platform = oauth1.Platform(
     'https://api.twitter.com/oauth/request_token',
     'https://api.twitter.com/oauth/authorize',
@@ -50,53 +53,73 @@ class _MyWidgetState extends State<MyWidget> {
   @override
   void initState() {
     super.initState();
+    _load();
+  }
+
+  Future<void> _load() async {
+    final client = oauth1.Client(
+      platform.signatureMethod,
+      clientCredentials,
+      oauth1.Credentials(
+        'Twitter Access Token をここに設定',
+        'Twitter Access Token Secret をここに設定',
+      ),
+    );
+    final apiResponse = await client.get(
+      Uri.parse(
+        //'https://api.twitter.com/1.1/statuses/home_timeline.json?count=30'),
+        'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterjp&count=30'),
+        //'https://api.twitter.com/1.1/search/tweets.json?q=flutter&lang=ja&count=30'),
+    );
+    final data = json.decode(apiResponse.body);
+
+    setState((){
+      final items = data as List;
+      //List<Item> _items = <Item>[];
+      items.forEach((dynamic element) {
+        final issue = element as Map;
+        _items.add(Item(
+          //id_str: issue['id_str'] as String,
+          text:   issue['text'] as String,
+        ));
+      });
+    });
+    //print(apiResponse.body);
+    //print(_items[0].text);
+    //print(_items[1].text);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                // Access Token を使ってAPIにリクエストできる
-                final client = oauth1.Client(
-                  platform.signatureMethod,
-                  clientCredentials,
-                  oauth1.Credentials(
-                    'Twitter Access Token をここに設定',
-                    'Twitter Access Token Secret をここに設定',
-                  ),
-                );
-                final apiResponse = await client.get(
-                  Uri.parse(
-                      //'https://api.twitter.com/1.1/statuses/home_timeline.json?count=1'),
-                      //'https://api.twitter.com/1.1/search/tweets.json?q=flutter&lang=ja&count=1'),
-                      'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterjp&count=2'),
-                );
-
-                final data = json.decode(apiResponse.body);
-                final items = data as List;
-                List<Item> _items = <Item>[];
-                items.forEach((dynamic element) {
-                  final issue = element as Map;
-                  _items.add(Item(
-                    id_str: issue['id_str'] as String,
-                    text:   issue['text'] as String,
-                  ));
-                });
-
-                //print(apiResponse.body);
-                print(_items[0].text);
-                print(_items[1].text);
-              },
-              child: Text('OK'),
-            ),
-          ],
-        ),
-      ),
+    return CupertinoPageScaffold(
+      child: (_items == null || _items.length == 0)
+        ? Text("Loading....", style: _buildTextStyle())
+        : Column(children: [
+            Flexible(
+              child: ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  final issue = _items[index];
+                  return Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(issue.text!, style: _buildTextStyle())
+                      )
+                    ]
+                  );
+                }
+              )
+            )
+         ])
     );
   }
+}
+
+var myTextStyle = TextStyle();
+TextStyle _buildTextStyle() {
+  return myTextStyle = TextStyle(
+    fontWeight: FontWeight.w100,
+    decoration: TextDecoration.none,
+    fontSize: 16,
+    color: CupertinoColors.black,
+  );
 }
