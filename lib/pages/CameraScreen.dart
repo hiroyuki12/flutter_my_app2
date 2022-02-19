5fc12087e8c89e388f56260b162c4cad5d1aa63c
hiroyuki12 <hiroyuki12@gmail.com>
2022-02-19 22:10:57 +0900

    Update CameraScreen CupertinoPage.

--- lib/pages/CameraScreen.dart
+++ lib/pages/CameraScreen.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:image_picker/image_picker.dart';
+import 'DarkModeColor.dart';
 
 //void main() => runApp(MyApp());
 
@@ -34,20 +36,38 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Image Picker Sample'),
+    isDarkMode = true;
+    return CupertinoPageScaffold(
+      backgroundColor: isDarkMode ? darkModeBackColor : backColor,
+      navigationBar: CupertinoNavigationBar(
+        backgroundColor: isDarkMode ? darkModeBackColor : backColor,
+        middle: Text("Image Picker Sample", style: _buildTextStyle()),
+        //trailing: Text("Edit", style: myTextStyle),
       ),
-      body: Center(
-        child: _image == null
-            ? Text('No image selected.')
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _image == null
+            ? Text('No image selected.', style: _buildTextStyle())
             : Image.file(_image!),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: getImage,
-        child: Icon(Icons.add_a_photo),
+          CupertinoButton(
+            onPressed: getImage,
+            child: Icon(Icons.add_a_photo),
+          ),
+        ],
       ),
     );
   }
+}
+
+var myTextStyle = new TextStyle();
+TextStyle _buildTextStyle() {
+  return myTextStyle = new TextStyle(
+  fontWeight: FontWeight.w100,
+  decoration: TextDecoration.none,
+  fontSize: 16,
+  color: isDarkMode ? darkModeForeColor : foreColor,  //black , darkMode=white
+  );
 }
 
