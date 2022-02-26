ac3d16919016f6a826fd0c5977f228d32d3b2ef6
hiroyuki12 <hiroyuki12@gmail.com>
2022-02-26 21:30:19 +0900

    Update main.dart coc-flutter.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/cupertino.dart';
-//import 'package:firebase_core/firebase_core.dart';
-import 'pages/QiitaScreen.dart';
+import 'pages/qiitaScreen.dart';
 import 'pages/MastodonScreen.dart';
 import 'pages/Cupertino/CupertinoMenu.dart';
 import 'pages/Cupertino/CupertinoSub.dart';
 import 'pages/Basics/BasicsMenu.dart';
-import 'pages/CupertinoWebView.dart';
 import 'pages/TwitterScreen.dart';
 import 'pages/FeedlyScreen.dart';
-//import 'pages/Firebase/CupertinoFirebase.dart';
 
 void main() {
   //WidgetsFlutterBinding.ensureInitialized();
@@ -43,74 +40,74 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
+      navigationBar: const CupertinoNavigationBar(
         middle: Text("main", style: TextStyle(color: CupertinoColors.white)),
-        backgroundColor: const Color(0xff333333),
+        backgroundColor: Color(0xff333333),
       ),
       child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             CupertinoButton(
-                child: Text("flutter.dev(外部サイト)"),
+                child: const Text("flutter.dev(外部サイト)"),
                 onPressed: () async {
                   await _launchURL('https://flutter.dev');
                 }),
             CupertinoButton(
-                child: Text("環境構築(外部サイト)"),
+                child: const Text("環境構築(外部サイト)"),
                 onPressed: () async {
                   await _launchURL(
                       'https://mbp.hatenablog.com/entry/2019/12/29/000000?_ga=2.97341151.1500483094.1644030611-1356087000.1644030611');
                 }),
             CupertinoButton(
-                child: Text("flutter_my_app2(外部サイト)"),
+                child: const Text("flutter_my_app2(外部サイト)"),
                 onPressed: () async {
                   await _launchURL(
                       'https://mbp.hatenablog.com/entry/2022/02/05/143159');
                 }),
             CupertinoButton(
-              child: Text("Cupertino Menu ->"),
+              child: const Text("Cupertino Menu ->"),
               onPressed: () => Navigator.push(context,
                   MaterialPageRoute(builder: (context) => CupertinoMenu())),
             ),
             CupertinoButton(
-              child: Text("Basics Menu ->"),
+              child: const Text("Basics Menu ->"),
               onPressed: () => Navigator.push(context,
                   MaterialPageRoute(builder: (context) => BasicsMenu())),
             ),
             CupertinoButton(
-                child: Text("Zenn(Flutter) 外部サイト"),
+                child: const Text("Zenn(Flutter) 外部サイト"),
                 onPressed: () async {
                   await _launchURL(
                       'https://zenn.dev/topics/flutter?order=latest');
                 }),
             CupertinoButton(
-                child: Text("Qiita(Flutter) 外部サイト"),
+                child: const Text("Qiita(Flutter) 外部サイト"),
                 onPressed: () async {
                   await _launchURL('https://qiita.com/tags/flutter');
                 }),
             CupertinoButton(
-                child: Text("はてなブログタグ(Flutter) 外部サイト"),
+                child: const Text("はてなブログタグ(Flutter) 外部サイト"),
                 onPressed: () async {
                   await _launchURL('https://d.hatena.ne.jp/keyword/flutter');
                 }),
             CupertinoButton(
-              child: Text("Qiita(API)"),
+              child: const Text("Qiita(API)"),
               onPressed: () => Navigator.push(
                   context, MaterialPageRoute(builder: (context) => Qiita())),
             ),
             CupertinoButton(
-              child: Text("Mstdn(API)"),
+              child: const Text("Mstdn(API)"),
               onPressed: () => Navigator.push(
                   context, MaterialPageRoute(builder: (context) => Mastodon())),
             ),
             CupertinoButton(
-              child: Text("Twitter(API)(Need API KEY)"),
+              child: const Text("Twitter(API)(Need API KEY)"),
               onPressed: () => Navigator.push(
                   context, MaterialPageRoute(builder: (context) => Twitter())),
             ),
             CupertinoButton(
-              child: Text("Feedly(API)(Need Developer Token)"),
+              child: const Text("Feedly(API)(Need Developer Token)"),
               onPressed: () => Navigator.push(
                   context, MaterialPageRoute(builder: (context) => Feedly())),
             ),
@@ -120,7 +117,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   MaterialPageRoute(builder: (context) => CupertinoFirebase())),
             ),*/
             CupertinoButton(
-              child: Text("sub ->"),
+              child: const Text("sub ->"),
               onPressed: () => Navigator.push(context,
                   MaterialPageRoute(builder: (context) => CupertinoSub())),
             ),
